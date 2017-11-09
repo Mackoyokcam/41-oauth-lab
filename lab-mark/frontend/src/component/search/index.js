972641366ae22590bf6ff60f0a5dea74194ea06d
mackoyokcam <mark.hangarter@gmail.com>
2017-11-08 20:46:20 -0800

    code review edits

--- lab-mark/frontend/src/component/search/index.js
+++ lab-mark/frontend/src/component/search/index.js
@@ -7,29 +7,19 @@ import SearchForm from '../search-form'
 import * as charity from '../../action/charity.js'
 
 class Search extends React.Component {
-  constructor(props) {
-    super(props)
 
-    this.handleSearch = this.handleSearch.bind(this)
-    this.handlChangePage = this.handleChangePage.bind(this)
-  }
-
-  handleSearch(query) {
-    this.props.search(query)
-  }
-
-  handleChangePage(url) {
-    this.props.changePage(url)
+  componentWillMount() {
+    this.props.search()
   }
 
   render(){
 
-    let {charities} = this.props
+    let {charities, search, changePage} = this.props
     let count = Math.floor(charities.count / 10)
     return (
       <div className='search'>
         <h2> Search for Charities </h2>
-        <SearchForm onComplete={this.handleSearch} />
+        <SearchForm onComplete={search} />
         {charities.data.map((charity, i) =>
           <Charity
             charity={charity}
@@ -37,9 +27,9 @@ class Search extends React.Component {
           />
         )}
         {charities.data.length ?
-          <p> <button onClick={() => {this.handleChangePage(charities.links.prev)}}>Previous</button>
-            <button onClick={() => {this.handleChangePage(charities.links.next)}}>Next</button>
-            <button onClick={() => {this.handleChangePage(charities.links.last)}}>Last</button> </p>
+          <p> <button onClick={() => changePage(charities.links.prev)}>Previous</button>
+            <button onClick={() => changePage(charities.links.prev)}>Next</button>
+            <button onClick={() => changePage(charities.links.prev)}>Last</button> </p>
           : <p> No results </p>}
       </div>
     )
